ca68961438a9e840bbe192560cfb92de321bf8d0
Leonardo Mendes Castilho <leonardo.smendescastilho@gmail.com>
2025-05-02 22:06:53 -0300

    feat: Implementa config react query e aplica

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
 import { BrowserRouter } from 'react-router-dom'
 
 import { RouterProvider } from './route/RouterProvider'
 
 export const App = () => {
   return (
-    <BrowserRouter>
-      <RouterProvider />
-    </BrowserRouter>
+    <QueryClientProvider client={new QueryClient()}>
+      <BrowserRouter>
+        <RouterProvider />
+      </BrowserRouter>
+    </QueryClientProvider>
   )
 }
