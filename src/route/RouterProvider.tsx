128b15e8c2c352153bddea0e7c429158dfb23977
Leonardo Mendes Castilho <leonardo.smendescastilho@gmail.com>
2025-05-06 19:51:00 -0300

    feat: Implementa ProtectedRoutes

--- src/route/RouterProvider.tsx
+++ src/route/RouterProvider.tsx
@@ -2,12 +2,18 @@ import { Route, Routes } from 'react-router-dom'
 
 import Layout from '@/pages/@layout/Layout.tsx'
 import { Home } from '@/pages/home'
+import { Login } from '@/pages/login'
+import { ProtectedRoutes } from '@/route/ProtectedRoutes'
 
 export const RouterProvider = () => {
   return (
     <Routes>
+      <Route path='/login' element={<Login />} />
+     
+      <Route element={<ProtectedRoutes />}>
       <Route path='/' element={<Layout />}>
         <Route index element={<Home />} />
+      </Route>
       </Route>
     </Routes>
   )
