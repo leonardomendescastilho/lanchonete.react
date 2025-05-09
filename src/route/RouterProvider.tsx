4c78d0e3ecf4dd34855ba69bbb861fb187b69397
Leonardo Mendes Castilho <leonardo.smendescastilho@gmail.com>
2025-05-09 08:10:45 -0300

    feat: Implementa rota para o cadastro

--- src/route/RouterProvider.tsx
+++ src/route/RouterProvider.tsx
@@ -4,11 +4,13 @@ import Layout from '@/pages/@layout/Layout.tsx'
 import { Home } from '@/pages/home'
 import { Login } from '@/pages/Login'
 import { ProtectedRoutes } from '@/route/ProtectedRoutes'
+import { SignUp } from '@/pages/SignUp'
 
 export const RouterProvider = () => {
   return (
     <Routes>
       <Route path='/login' element={<Login />} />
+      <Route path='/cadastrar' element={<SignUp />} />
      
       <Route element={<ProtectedRoutes />}>
       <Route path='/' element={<Layout />}>
