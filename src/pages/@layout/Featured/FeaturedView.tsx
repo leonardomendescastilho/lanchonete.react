8b311becdfebf83c55755a06d7e9a1df22ed269f
Leonardo Mendes Castilho <leonardo.smendescastilho@gmail.com>
2025-05-11 20:53:36 -0300

    style: Ajusta layout

--- src/pages/@layout/Featured/FeaturedView.tsx
+++ src/pages/@layout/Featured/FeaturedView.tsx
@@ -1,13 +1,11 @@
-import { Box, Container } from '@mui/material'
+import { Container } from '@mui/material'
 
 import { CardFeatured } from '@/components/Card/CardFeatured'
 
 export const FeaturedView = () => {
   return (
-    <Box component={'section'} sx={{ paddingY: '100px' }}>
-      <Container>
-        <CardFeatured />
-      </Container>
-    </Box>
+    <Container component={'section'}>
+      <CardFeatured />
+    </Container>
   )
 }
