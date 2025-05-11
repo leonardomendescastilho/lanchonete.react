d80b3671a95a7a7485859ff1986625a332ad84cd
Leonardo Mendes Castilho <leonardo.smendescastilho@gmail.com>
2025-05-11 20:55:59 -0300

    feat: Implementa rota menu

--- src/route/RouterProvider.tsx
+++ src/route/RouterProvider.tsx
@@ -1,21 +1,23 @@
 import { Route, Routes } from 'react-router-dom'
 
 import Layout from '@/pages/@layout/Layout.tsx'
-import { Home } from '@/pages/home'
 import { Login } from '@/pages/Login'
-import { ProtectedRoutes } from '@/route/ProtectedRoutes'
 import { SignUp } from '@/pages/SignUp'
+import { Home } from '@/pages/home'
+import { Menu } from '@/pages/menu'
+import { ProtectedRoutes } from '@/route/ProtectedRoutes'
 
 export const RouterProvider = () => {
   return (
     <Routes>
       <Route path='/login' element={<Login />} />
       <Route path='/cadastrar' element={<SignUp />} />
-     
+
       <Route element={<ProtectedRoutes />}>
-      <Route path='/' element={<Layout />}>
-        <Route index element={<Home />} />
-      </Route>
+        <Route path='/' element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path='menu' element={<Menu />} />
+        </Route>
       </Route>
     </Routes>
   )
