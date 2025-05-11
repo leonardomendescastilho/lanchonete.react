f43625b68a803ece323464855b82562ca40e5334
Leonardo Mendes Castilho <leonardo.smendescastilho@gmail.com>
2025-05-11 20:56:23 -0300

    style: Ajusta layout

--- src/pages/home/HomeView.tsx
+++ src/pages/home/HomeView.tsx
@@ -1,15 +1,15 @@
 import { Box } from '@mui/material'
 
+import { Catalog } from '../@layout/Catalog'
 import { Featured } from '../@layout/Featured'
 import { Hero } from '../@layout/Hero'
-import { Menu } from '../@layout/Menu'
 
 export const HomeView = () => {
   return (
-    <Box>
+    <Box sx={{ display: 'flex', flexDirection: 'column', gap: 20, marginBottom: '100px' }}>
       <Hero />
       <Featured />
-      <Menu />
+      <Catalog />
     </Box>
   )
 }
